7607807b91c52874c40018d44585a606697e1b03
mdnahidooo <md.nahidofficial.cse@gmail.com>
2026-05-21 06:04:37 +0600

    git push for deploy the project

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const verifyToken = async (req, res, next) => {
 
 async function run() {
     try {
-        await client.connect();
+        // await client.connect();
 
 
         const db = client.db("libra-room-db");
@@ -283,7 +283,7 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
